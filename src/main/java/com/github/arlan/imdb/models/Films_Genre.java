a7b258f2c0df19cd44ef1960bc5660d76cb16ac5
Light <ismuhanov.2015@mail.ru>
2020-04-09 18:38:15 +0600

    Films_Genre

---
+++ src/main/java/com/github/arlan/imdb/models/Films_Genre.java
@@ -0,0 +1,46 @@
+package com.github.arlan.imdb.models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "films_genre")
+public class Films_Genre {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(foreign = true, foreignAutoCreate = true, foreignAutoRefresh = true, columnName = "genre_id")
+    private int genre_id;
+    @DatabaseField(foreign = true, foreignAutoCreate = true, foreignAutoRefresh = true, columnName = "films_id")
+    private int films_id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGenre_id() {
+        return genre_id;
+    }
+
+    public void setGenre_id(int genre_id) {
+        this.genre_id = genre_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFilms_id(int films_id) {
+        this.films_id = films_id;
+    }
+
+    public int getFilms_id() {
+        return films_id;
+    }
+
+    Films_Genre() {
+    }
+    Films_Genre(int id,int genre_id,int films_id) {
+        this.films_id = films_id;
+        this.id = id;
+        this.genre_id = genre_id;
+    }
+}
